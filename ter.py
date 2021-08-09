4d4ae077e6bb18798d79c9edbc0b85c9eb9343d1
erwinmaxwell <56914535+erwinmaxwell@users.noreply.github.com>
2021-08-10 01:35:54 +0200

    premier script python
    peut updater les données et les parser avec pandas, fonction pour créer un pseudo graphe mais super sous optimale (et du coup impossible à run)

---
+++ ter.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+import os
+import requests
+import zipfile
+from tqdm import tqdm
+
+#update les données si : pas dans dossier ou n'importe quel argument en executant le script
+larg=[]
+for arg in sys.argv:
+    larg.append(arg)
+if not os.path.isfile("data.zip") or len(larg)>1:
+    url="https://eu.ftp.opendatasoft.com/sncf/gtfs/export-ter-gtfs-last.zip"
+    r = requests.get(url)  
+    with open('data.zip', 'wb') as f:
+            f.write(r.content)
+    with zipfile.ZipFile("data.zip", 'r') as zip_ref:
+            zip_ref.extractall()
+
+#le parseur de pandas en fait il marche bien
+calendar_dates=pd.read_csv("calendar_dates.txt")
+stops=pd.read_csv("stops.txt")
+stop_times=pd.read_csv("stop_times.txt")
+routes=pd.read_csv("routes.txt")
+transfers=pd.read_csv("transfers.txt")
+trips=pd.read_csv("trips.txt")
+
+#du coup on fait une liste de chaque arrêt avec leurs id, nom et position et on initie un dictionaire qui à chaque arrêt va donner la liste de ses voisins 
+stoppointsfull=stops.groupby("location_type").get_group(0)
+stoppoints=stoppointsfull[["stop_id","stop_name","stop_lon","stop_lat"]]
+neighbours=dict(zip([i for i in stoppoints.stop_id],[[] for i in stoppoints.stop_id]))
+
+# et maintenant on créé une fonction qui itere de manière bien degueulasse sur chacun des 230244 trajets individuels, et on prend la liste ordonnée de ses arrêts, puis on en tire les voisins et on en remplit le dictionaire neighbours créé plus haut pour l'arrêt en question
+# le tqdm il sert à avoir une barre de chargement pour se rendre compte que sur mon pc ça prendrait 6h de calculs
+pastgsids=[]
+def get_neighbours():
+    for i in tqdm(stop_times.trip_id):
+        g=stop_times.groupby('trip_id').get_group(i)
+        gsid=[]
+        for sid in g.stop_id:
+            gsid.append(sid)
+        if set(gsid) not in pastgsids:
+            for i,n in zip(gsid,range(len(gsid))):
+                if n==0:
+                    neighbours[i].append(gsid[1])
+                if n==len(gsid)-1:
+                    neighbours[i].append(gsid[-2])
+                else:
+                    neighbours[i].append(gsid[n+1])
+                    neighbours[i].append(gsid[n-1])
+        pastgsids.append(set(gsid))
+
+get_neighbours()
+
+#joli gui pour visualiser les données si jamais
+#from pandasgui import show
+#show(stops,stop_times,calendar_dates,transfers,routes,trips)
